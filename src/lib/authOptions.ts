4c52d84972918a359947dc24964ae633559c840f
Vladislav Li <vladislav.li911@gmail.com>
2023-12-12 04:20:06 +0900

    Delete authOptions.ts file

--- src/lib/authOptions.ts
+++
@@ -1,8 +0,0 @@
-import { NextAuthOptions } from "next-auth";
-export const authOptions: NextAuthOptions = {
-  debug: false,
-  session: {
-    strategy: "jwt",
-  },
-  providers: [SteamProvider(_, { c })],
-};

e2decf3a9495d6bbd4f1e6570457829b8d89187f
Vladislav Li <vladislav.li911@gmail.com>
2024-01-31 21:22:25 +0900

    Add HeaderValue component and update table headers

---
+++ src/components/HeaderValue.tsx
@@ -0,0 +1,20 @@
+import { cn } from "@/lib/utils";
+
+type Props = {
+  className?: string;
+  header: string;
+  value: string | number | null | undefined;
+};
+export default function HeaderValue({ className, header, value }: Props) {
+  return (
+    <section
+      className={cn(
+        "flex w-fit rounded-large border border-divider",
+        className
+      )}
+    >
+      <div className="rounded-l-large bg-content1 p-2">{header}</div>
+      <div className="rounded-r-large bg-content2 p-2">{value}</div>
+    </section>
+  );
+}

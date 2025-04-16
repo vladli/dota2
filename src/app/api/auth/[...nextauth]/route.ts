b847ccfaa6ad2e9eda32a88a97a0e3e637d303a5
Vladislav Li <vladislav.li911@gmail.com>
2025-04-16 13:58:34 +0500

    Refactor route handler to use NextApiRequest and NextApiResponse

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,10 @@
-import { NextRequest } from "next/server";
-import NextAuth from "next-auth";
+import { NextApiRequest, NextApiResponse } from "next/types";
+import NextAuth from "next-auth/next";
 
 import { getAuthOptions } from "@/lib/authOptions";
 
-async function handler(req: NextRequest) {
-  //@ts-ignore
-  return NextAuth(req, undefined, getAuthOptions(req));
-}
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
+  return NextAuth(req, res, getAuthOptions(req));
+};
 
 export { handler as GET, handler as POST };

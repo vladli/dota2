94fe648d24ac1a7e61c7ca0a60e4bfe2a2dfab31
Vladislav Li <vladislav.li911@gmail.com>
2025-04-16 13:41:03 +0500

    Update pnpm-lock.yaml to relax @types/react version constraints

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXT_PUBLIC_STRATZ_API: process.env.NEXT_PUBLIC_STRATZ_API,
+    STEAM_SECRET: process.env.STEAM_SECRET,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
